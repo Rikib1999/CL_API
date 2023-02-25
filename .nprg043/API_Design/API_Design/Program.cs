304fb780e8781c88284a5a98479cadbc2981ecff
kapustb <boriskokap@gmail.com>
2023-02-25 15:09:53 +0100

    Initial Creation

---
+++ .nprg043/API_Design/API_Design/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
